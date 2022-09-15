a19479be45a01da39f977a6d7713db4a933c21e1
Manchips <78320081+Manchips@users.noreply.github.com>
2022-09-15 01:10:09 -0700

    Attempting to fix issues
    Do not push or pull, need to figure out some issues I'm having will attempt to make simple functionality for deck and grave

---
+++ CardGameProject/Assets/ModAssets/Scripts/Ui/UI Manager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public int deck = 20; //Deck starting size, this is temp
+
+    public int grave = 0; //Graveyard should start at 0 
+
+    public int actions = 3; //Current testing will be done with player able to perform 3 actions
+
+    public int pScore;
+
+    public int eScore;
+
+    public int gold = 50; //For testing allow player to start with gold.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
