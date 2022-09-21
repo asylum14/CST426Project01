59e2ad6952f7befe5dccbbc0c955d9e523b43738
asylum14 <michaelanthonysilva18@gmail.com>
2022-09-21 13:16:22 -0700

    Squashed commit of the following:
    commit 31ae1abf2e5ace8b657c9884370fcbbb09e4ed5b
    Author: Stumpkin <banksjalen1@gmail.com>
    Date:   Thu Sep 15 08:26:13 2022 -0700
    
        Go on and take it
        New feature so that you can draw a card from the deck
        Added new UI for displying the element's name and the atomic number
        Added: SampleUI.cs - For all the NEW UI code
        Mod: ItemSpawner.cs - Implemented a draw feature and place cards in
        their missing hand slot
        Mod: RaycastThing.cs - Grabs the text from the object then places it in SampleUI
    
    commit 337f53f4a84c4c2c51194f69f5ac724f7f9a244c
    Author: Stumpkin <banksjalen1@gmail.com>
    Date:   Tue Sep 13 09:37:27 2022 -0700
    
        The Deck update
        Create a hand so that you can hold 5 cards from the deck
        The deck is shuffled then deals 5 cards from the deck to your hand upon
        start up
        CURRENT BUG: The raycast can stop working if you switch inbetween scenes
        often
        CURRENT BUG: Sometime you can be dealt with the same card multiple times
        where idealy there is one card for each type currently
    
    commit 71364459032abce830ba37c828e17c2752a3bdbd
    Author: Stumpkin <banksjalen1@gmail.com>
    Date:   Thu Sep 8 12:40:26 2022 -0700
    
        THE PURGE
        I got frustrated with the files so I made actions on my own... So here's my progress from the my other repo...
    
    commit f4b977e877a2426456df042744ea163093d6681d
    Author: Stumpkin <banksjalen1@gmail.com>
    Date:   Thu Sep 1 15:39:07 2022 -0700
    
        insert J-Branch here

---
+++ CardGameProject/Assets/Scripts/RaycastThing.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastThing : MonoBehaviour
+{
+    public ItemSpawner boss;
+    public SampleUI ui;
+    Camera camera;
+    RaycastHit hit;
+    Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        mousePos = camera.ScreenToWorldPoint(mousePos);
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray,out hit, 100f))
+            {
+                string name = hit.transform.gameObject.GetComponent<Card>().getName();
+                int number = hit.transform.gameObject.GetComponent<Card>().getNumber();
+                Debug.Log(number + " " + name);
+                ui.setElementText(number, name);
+                if (Input.GetMouseButton(0))
+                {
+                    boss.removeCard(hit.transform.gameObject);
+                }
+            }
+
+        // }
+    }
+}
---
+++ CardGameProject/Assets/Scripts/SampleUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class SampleUI : MonoBehaviour
+{
+    private TextMeshProUGUI someText;
+    private int anGrabber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anGrabber = 0;
+        someText = GetComponent<TextMeshProUGUI>();
+        someText.text = "";
+    }
+
+    public void setElementText(int an, string text)
+    {
+        string number = an.ToString();
+        someText.text = (number + " " + text); 
+    }
+
+    public void setText(string text)
+    {
+        someText.text = text;
+    }
+}
